3ee77f52e470bad8bbf6f6f241455ff697972d7e
Rich Harrison <r.harrison891@gmail.com>
2025-11-19 11:08:53 +0000

    update

--- main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("onload", (event) => {
-  fetch("/partials/header.html")
-        .then(r => r.text())
-        .then(html => document.getElementById("header").outerHTML = html);
-
-      fetch("/partials/footer.html")
-        .then(r => r.text())
-        .then(html => document.getElementById("footer").outerHTML = html);
-});
