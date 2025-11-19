00e8960e2e10a61303e173f3d54e827dabcf1c6e
Rich Harrison <r.harrison891@gmail.com>
2025-11-19 10:58:11 +0000

    update

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-fetch("/partials/header.html")
-    .then(r => r.text())
-    .then(html => document.getElementById("header").outerHTML = html);
+document.onload=function(){
+    fetch("/partials/header.html")
+        .then(r => r.text())
+        .then(html => document.getElementById("header").outerHTML = html);
 
-  fetch("/partials/footer.html")
-    .then(r => r.text())
-    .then(html => document.getElementById("footer").outerHTML = html);
+      fetch("/partials/footer.html")
+        .then(r => r.text())
+        .then(html => document.getElementById("footer").outerHTML = html);
+    }
